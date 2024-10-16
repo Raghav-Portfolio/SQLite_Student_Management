3caf63107683786b790c7465e1ca5612b913df90
Raghav-Git-Windows <rgs.rox@gmail.com>
2024-10-16 14:16:31 +0530

    Populated the table using dummy data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 class MainWindow(QMainWindow): #this class allows us to add functionalities like a tool bar, status bar, menu bar etc
     def __init__(self):
@@ -19,12 +20,22 @@ class MainWindow(QMainWindow): #this class allows us to add functionalities like
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(('ID','Name','Course','Mobile Number')) #  this method expects a tuple  
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
-        
+        self.load_data()
 
         
     def load_data(self):
-        self.table
+        """ Interact with the SQL Table """
+        connection = sqlite3.connect('database.db')
+        result = connection.execute("SELECT * FROM students")
+        
+        #populate the table with the data now
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for columm_number, data in enumerate(row_data):
+                self.table.setItem(row_number, columm_number,QTableWidgetItem(str(data)))
+        connection.close()
         
                 
 app = QApplication(sys.argv)        
